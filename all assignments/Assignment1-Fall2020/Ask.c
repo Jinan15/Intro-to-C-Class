3c981b3332deeb88791ecf23ec6c02fc21dc398f
Jinan Patel <pateljinan@gmail.com>
2020-09-13 01:55:58 -0400

    assignment
    ask.c week 1 hw

---
+++ all assignments/Assignment1-Fall2020/Ask.c
@@ -0,0 +1,22 @@
+/*
+    Name: Jinan Patel
+    Date: 8-26-2020
+    Assignment: Your First C Programs
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int width, height;
+    printf("Please enter a width: ");
+    scanf("%d", &width);
+    printf("Please enter a height: ");
+    scanf("%d", &height);
+    printf("\nThank You!!!\n\n");
+    printf("You entered A width of %d and a height of %d\n", width, height);
+
+    system("pause");
+
+    return 0;
+}
