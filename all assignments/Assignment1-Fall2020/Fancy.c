879ad729764d34b7dca2858f1c78dd9ef70e60ea
Jinan Patel <pateljinan@gmail.com>
2020-09-13 01:57:54 -0400

    assignment fancy
    week 1 assignment fancy.c

---
+++ all assignments/Assignment1-Fall2020/Fancy.c
@@ -0,0 +1,25 @@
+/*
+    Name: Jinan Patel
+    Date: 8-26-2020
+    Assignment: Your First C Programs
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("********************************************* \n");
+    printf("*                                           * \n");
+    printf("*       ******           ******             * \n");
+    printf("*      *      *         *      *            * \n");
+    printf("*     *  *  *  *       *  *  *  *           * \n");
+    printf("*     *   ||   *       *   ||   *           * \n");
+    printf("*      * \\--/ *         * \\--/ *            *\n");
+    printf("*       ******           ******             *\n");
+    printf("*                                           *\n");
+    printf("********************************************* \n");
+
+    system("pause");
+
+    return 0;
+}
