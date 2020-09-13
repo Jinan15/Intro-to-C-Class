ebd6e66616d2aedca2600d4b3f5e8757d2eea950
Jinan Patel <pateljinan@gmail.com>
2020-09-13 01:58:26 -0400

    Created WeatherTalk.c
    week 1 assignment WeatherTalk.c

---
+++ all assignments/Assignment1-Fall2020/WeatherTalk.c
@@ -0,0 +1,18 @@
+/*
+    Name: Jinan Patel
+    Date: 8-26-2020
+    Assignment: Your First C Programs
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("It is extremely hot outside today with temps exceeding 90 degrees. \n");
+    printf("The humidity outside today is 70%. \n");
+    printf("Today is a cloudy day with some isolated thunderstorms \n");
+
+    system("pause");
+
+    return 0;
+}
