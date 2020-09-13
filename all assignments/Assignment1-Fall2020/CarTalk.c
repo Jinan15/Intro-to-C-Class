67de41371a728de4cab195c64c196e83dbef0b25
Jinan Patel <pateljinan@gmail.com>
2020-09-13 01:56:54 -0400

    assignment carTalk
    assignment carTalk week 1

---
+++ all assignments/Assignment1-Fall2020/CarTalk.c
@@ -0,0 +1,18 @@
+/*
+    Name: Jinan Patel
+    Date: 8-26-2020
+    Assignment: Your First C Programs
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("I have two cars: one A white GMC Yukon and a silver Honda Odyssey. \n");
+    printf("Because the Honda Odyssey is 14 years old and the AC is not working I do not like driving it but I love the GMC Yukon. \n");
+    printf("My dad decided to add 1500 watt speakers with subwoofers in the back of the Yukon! \n");
+
+    system("pause");
+
+    return 0;
+}
